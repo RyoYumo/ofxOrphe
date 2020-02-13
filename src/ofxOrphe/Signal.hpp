23643a80bcce59ce304b406f0c02a074a8a8ceb2
ryoyumo <yumo.after.fun@gmail.com>
2020-02-13 20:04:31 +0900

    add the function to call multiple callbacks

--- src/ofxOrphe/Signal.hpp
+++ src/ofxOrphe/Signal.hpp
@@ -8,26 +8,30 @@
 #define OFX_ORPHE_SIGNAL_HPP_
 
 #include <functional>
+#include <vector>
 #include "ofEvents.h"
 
+namespace ofx{
+namespace orphe{
 template<typename T>
 class Signal {
 public:
-    Signal() { ofAddListener(event_, this, &Signal::callback);}
-    ~Signal(){ ofRemoveListener(event_, this, &Signal::callback);}
+    using callback_type = std::function<void(T&)>;
+    Signal() { ofAddListener(event_, this, &Signal::call);}
+    ~Signal(){ ofRemoveListener(event_, this, &Signal::call);}
     
     template<typename Callback>
     void connect(Callback&& callback){
-        callback_obj_ = std::forward<Callback>(callback);
+        callback_collection_.push_back(std::forward<Callback>(callback));
     }
     
     void emit(T& arg){ ofNotifyEvent(event_, arg);}
 private:
     ofEvent<T> event_;
-    std::function<void(T&)> callback_obj_;
-    void callback(T& arg){
-        if(callback_obj_){
-            callback_obj_(arg);
+    std::vector<callback_type> callback_collection_;
+    void call(T& arg){
+        for(auto& c : callback_collection_){
+            if(c){ c(arg); }
         }
     }
 };
@@ -35,24 +39,26 @@ private:
 template<>
 class Signal<void> {
 public:
-    Signal() { ofAddListener(event_, this, &Signal::callback);}
-    ~Signal(){ ofRemoveListener(event_, this, &Signal::callback);}
+    using callback_type = std::function<void(void)>;
+    Signal() { ofAddListener(event_, this, &Signal::call);}
+    ~Signal(){ ofRemoveListener(event_, this, &Signal::call);}
     
     template<typename Callback>
     void connect(Callback&& callback){
-        callback_obj_ = std::forward<Callback>(callback);
+        callback_collection_.push_back(std::forward<Callback>(callback));
     }
     
     void emit(){ ofNotifyEvent(event_);}
     
 private:
     ofEvent<void> event_;
-    std::function<void(void)> callback_obj_;
-    void callback(){
-        if(callback_obj_){
-            callback_obj_();
+    std::vector<callback_type> callback_collection_;
+    void call(){
+        for(auto& c : callback_collection_){
+            if(c){ c(); }
         }
     }
 };
-
+} // namespace orphe
+} // namespace ofx
 #endif // OFX_ORPHE_SIGNAL_HPP_
