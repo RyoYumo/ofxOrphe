fe655ef19bf02adaaef2c7c8f49c0cf34bcacb5b
ryoyumo <yumo.after.fun@gmail.com>
2020-02-16 15:25:08 +0900

    change struct of sensor values

--- src/ofxOrphe/OrpheOne.hpp
+++ src/ofxOrphe/OrpheOne.hpp
@@ -9,38 +9,37 @@
 
 #include "glm/gtc/quaternion.hpp"
 #include "glm/glm.hpp"
+#include "Signal.hpp"
 #include "ofEvents.h"
 
 namespace ofx{
 namespace orphe{
 class OrpheOne {
 public:
-    const glm::quat& getOrientationQuat() const { return sensor_data_.orientation_quat; }
-    const glm::vec3& getOrientationEuler() const { return sensor_data_.orientation_euler; }
-    const glm::vec3& getAcceleration() const { return sensor_data_.accleration; }
-    const glm::vec3& getGyro() const { return sensor_data_.gyro; }
-    ofEvent<void>&   getHeelEvent() { return gesture_.heel;}
-    ofEvent<void>&   getStepEvent() { return gesture_.step;}
-    void             setOrientationQuat(const glm::quat& quat) { sensor_data_.orientation_quat = quat; }
-    void             setOrientationEuler(const glm::vec3& angle) { sensor_data_.orientation_euler = angle; }
-    void             setAccelaration(const glm::vec3& acc) { sensor_data_.accleration = acc; }
-    void             setGyro(const glm::vec3& gyro) { sensor_data_.gyro = gyro; }
-    void             heel() { ofNotifyEvent(gesture_.heel);}
-    void             step() { ofNotifyEvent(gesture_.step);}
+    void                setOrientation(const glm::vec3& euler) { sensor_.orientation = euler; orientation_sig_.emit(sensor_.orientation); }
+    void                setAcceleration(const glm::vec3& acc) { sensor_.acceleration = acc; acceleration_sig_.emit(sensor_.acceleration);}
+    void                setGyro(const glm::vec3& gyro) { sensor_.gyro = gyro; gyro_sig_.emit(sensor_.gyro); }
+    void                setManitude(float mag) { sensor_.magnitude = mag; magnitude_sig_.emit(sensor_.magnitude);}
+    const glm::vec3&    getOrientation() const { return sensor_.orientation; }
+    const glm::vec3&    getAcceleration() const { return sensor_.acceleration; }
+    const glm::vec3&    getGyro() const { return sensor_.gyro; }
+    const float         getMagnitude() const { return sensor_.magnitude; }
+    Signal<glm::vec3>&  getOrientationSignal() { return orientation_sig_; }
+    Signal<glm::vec3>&  getAccelerationSignal() { return acceleration_sig_; }
+    Signal<glm::vec3>&  getGyroSignal() { return gyro_sig_;}
+    Signal<float>&      getMagnitudeSignal() { return magnitude_sig_; }
 private:
     struct SensorData {
-        glm::quat orientation_quat;
-        glm::vec3 orientation_euler;
-        glm::vec3 accleration;
-        glm::vec3 gyro;
+           glm::vec3     orientation;
+           glm::vec3     acceleration;
+           glm::vec3     gyro;
+           float         magnitude;
     };
-    
-    struct Gesture {
-        ofEvent<void> heel;
-        ofEvent<void> step;
-    };
-    SensorData  sensor_data_;
-    Gesture     gesture_;
+    SensorData sensor_;
+    Signal<glm::vec3> orientation_sig_;
+    Signal<glm::vec3> acceleration_sig_;
+    Signal<glm::vec3> gyro_sig_;
+    Signal<float>     magnitude_sig_;
 };
 } // namespace orphe
 } // namespace ofx
