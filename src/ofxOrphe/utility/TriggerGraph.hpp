2dec19bf258ed326c4adc841c97c55d577e4eda3
ryoyumo <yumo.after.fun@gmail.com>
2020-02-25 00:09:36 +0900

    change name

--- src/ofxOrphe/utility/TriggerGraph.hpp
+++ src/ofxOrphe/utility/TriggerGraph.hpp
@@ -16,13 +16,13 @@ template<std::size_t BufferSize>
 class TriggerGraph {
 public:
     static constexpr auto size = BufferSize;
-    TriggerGraph(): history_(history_size), area_(0, 0, 600, 300),color_(ofColor::white){
+    TriggerGraph(): buffer_(size), area_(0, 0, 600, 300),color_(ofColor::white){
         ofAddListener(ofEvents().update, this, &TriggerGraph::update);
     }
     
     void setDrawingArea(const ofRectangle& area){ area_ = area;}
     void setColor(const ofColor& color){ color_ = color;}
-    void trigger(){ history_.back() = 1;}
+    void trigger(){ buffer_.back() = 1;}
     void draw(){
         ofPushStyle();
         ofNoFill();
@@ -34,12 +34,12 @@ public:
         ofPushMatrix();
         ofTranslate(area_.x, area_.y + area_.height);
         ofBeginShape();
-        for(auto i = 0; i < history_.size(); ++i){
+        for(auto i = 0; i < buffer_.size(); ++i){
             if(i == 0) ofVertex(0, 0);
-            auto x = i * ((float)area_.width / (history_.size()-1));
-            auto y = history_[i] == 1 ? area_.height * -0.8 : 0.0;
+            auto x = i * ((float)area_.width / (buffer_.size()-1));
+            auto y = buffer_[i] == 1 ? area_.height * -0.8 : 0.0;
             ofVertex(x,y);
-            if(i == history_.size() -1) ofVertex(area_.width, 0);
+            if(i == buffer_.size() -1) ofVertex(area_.width, 0);
         }
         ofEndShape();
         ofPopMatrix();
@@ -48,7 +48,7 @@ public:
     
     
 private:
-    void update(ofEventArgs&){ buffer_.push_back(0); history_.pop_front();}
+    void update(ofEventArgs&){ buffer_.push_back(0); buffer_.pop_front();}
     ofRectangle area_;
     std::deque<int> buffer_;
     ofColor color_;
