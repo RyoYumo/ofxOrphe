12af703e36a9e99a32448b84111662b2723139f5
ryoyumo <yumo.after.fun@gmail.com>
2020-02-25 00:21:06 +0900

    update example

--- example-speed/src/ofApp.cpp
+++ example-speed/src/ofApp.cpp
@@ -13,19 +13,19 @@ void ofApp::setup(){
     receiver.right(&right);
     receiver.subscribe(kReceivePort);
     
-    left_speed_hisotry.setArea(ofRectangle(0, 0, 600, 300));
-    left_speed_hisotry.setInputValueRange(0, 15);
+    l_speed_history.setDrawingArea(ofRectangle(0, 0, 600, 300));
+    l_speed_history.setInputValueRange(0, 15);
     
-    right_speed_hisotry.setArea(ofRectangle(0, 300, 600, 300));
-    right_speed_hisotry.setInputValueRange(0, 15);
+    r_speed_history.setDrawingArea(ofRectangle(0, 300, 600, 300));
+    r_speed_history.setInputValueRange(0, 15);
     
     left.getSpeedSignal().connect([&](float& speed){
-        left_speed_hisotry.value(speed);
+        l_speed_history.addValue(speed);
         ofxSendOsc("127.0.0.1", kSendPort, "/left/speed", speed);
     });
     
     right.getSpeedSignal().connect([&](float& speed){
-        right_speed_hisotry.value(speed);
+        r_speed_history.addValue(speed);
         ofxSendOsc("127.0.0.1", kSendPort, "/right/speed", speed);
     });
 }
@@ -38,11 +38,11 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     ofDrawBitmapString("LEFT", 20, 20);
-    ofDrawBitmapString(ofToString(left_speed_hisotry.getValue()), 20, 40);
+    ofDrawBitmapString(ofToString(l_speed_history.getValue()), 20, 40);
     ofDrawBitmapString("RIGHT", 20, 320);
-    ofDrawBitmapString(ofToString(right_speed_hisotry.getValue()), 20, 340);
-    left_speed_hisotry.draw();
-    right_speed_hisotry.draw();
+    ofDrawBitmapString(ofToString(r_speed_history.getValue()), 20, 340);
+    l_speed_history.draw();
+    r_speed_history.draw();
 }
 
 //--------------------------------------------------------------
