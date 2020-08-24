608f40b100460e3b91dd8cb21be98bd9c922c56b
ryoyumo <yumo.after.fun@gmail.com>
2020-08-24 22:24:00 +0900

    add addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,8 @@
+meta:
+	ADDON_NAME = ofxOrphe
+	ADDON_DESCRIPTION = Library for NNF orphe shoes
+	ADDON_AUTHOR = Ryo Yumoto
+	ADDON_TAGS = "utility"
+	ADDON_URL = https://github.com/RyoYumo/ofxOrphe
+
+common:
